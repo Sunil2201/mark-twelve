47f09129af737c1628a154d9c9fd202f129681fa
Sunil Ballani <ballanisunil123@gmail.com>
2022-09-10 01:42:57 +0530

    completed hypotenuse part

---
+++ hypotenuse.js
@@ -0,0 +1,32 @@
+const baseValue = document.querySelector('#baseValue');
+const heightValue = document.querySelector('#heightValue');
+const calculateBtn = document.querySelector('#isTriangle');
+const outputSection = document.querySelector('#outputBox');
+
+
+calculateBtn.addEventListener("click", checkValues)
+
+function calculateHypotenuse(val1, val2){
+    if(val1 <= 0){
+        outputSection.innerText = "Base value should be greater than zero"
+    }
+    else if(val2 <= 0){
+        outputSection.innerText = "Height value should be greater than zero"
+    }
+    else{
+        const ans = Math.sqrt(val1*val1 + val2*val2)
+        outputSection.innerText = `The length of hypotenuse is ${ans}`
+    }
+}
+
+function checkValues(){
+    const val1 = Number(baseValue.value)
+    const val2 = Number(heightValue.value)
+    
+    if(val1 && val2){
+        calculateHypotenuse(val1, val2)
+    }
+    else{
+        outputSection.innerText = "Please enter base value & height value"
+    }
+}
