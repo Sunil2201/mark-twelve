9f31bf9b07d0fc52401aff9eea5703710d442eb5
Sunil Ballani <ballanisunil123@gmail.com>
2022-09-10 01:59:38 +0530

    completed area section

---
+++ area.js
@@ -0,0 +1,32 @@
+const baseValue = document.querySelector('#baseValue');
+const heightValue = document.querySelector('#heightValue');
+const calculateBtn = document.querySelector('#isTriangle');
+const outputSection = document.querySelector('#outputBox');
+
+
+calculateBtn.addEventListener("click", checkValues)
+
+function calculateArea(val1, val2){
+    if(val1 <= 0){
+        outputSection.innerText = "Base value should be greater than zero"
+    }
+    else if(val2 <= 0){
+        outputSection.innerText = "Height value should be greater than zero"
+    }
+    else{
+        const ans = 0.5*(val1*val2)
+        outputSection.innerText = `The area of triangle is ${ans} cm(squared)`
+    }
+}
+
+function checkValues(){
+    const val1 = Number(baseValue.value)
+    const val2 = Number(heightValue.value)
+    
+    if(val1 && val2){
+        calculateArea(val1, val2)
+    }
+    else{
+        outputSection.innerText = "Please enter base value & height value"
+    }
+}
